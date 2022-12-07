2fee90e42bbbd3c476f2581dcdbe8be044a04b84
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2022-12-07 19:58:30 -0300

    subiendo

--- poo/src/entidades/operaciones.java
+++ poo/src/entidades/operaciones.java
@@ -1,8 +1,3 @@
-//Crar una clase rectangulo que contenga atributos privados de base y altura
-//la clase incluira un metodo para crear el rectangulo con los datos del rectangulo
-//dados por el usuario.  tambien incluira un metodo para calcular el area
-//del rectangulo. se deberan definir los metodos getter y setter y
-//constructores correspondientes. area = base * altura.
 
 package entidades;
 
