4bfe8fd7c82004bfe51a09848f9c65fba3d8d2d3
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2023-05-23 17:06:14 -0300

    ac

--- Agenda-contactos/src/main/java/com/example/Agendacontactos/entidad/contacto.java
+++ Agenda-contactos/src/main/java/com/example/Agendacontactos/entidad/contacto.java
@@ -1,2 +1,103 @@
-package com.example.Agendacontactos.entidad;public class contacto {
+package com.example.Agendacontactos.entidad;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+//notacion entidad y nombre tabla
+@Entity
+@TableGenerator(name = "contactos")
+public class contacto {
+    //id con autoincremento. no hace falta nombrar las tablas esta vez
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //atributos
+    private Integer id;
+
+    @NotBlank(message = "Ingresar nombre: ")
+    private String nombre;
+
+    @NotBlank(message = "Ingresar telefono: ")
+    private String telefono;
+
+    @NotBlank(message = "Ingresar correo: ") //mesaje a validar en html.
+    @Email //validacion. para que contenga si o si una @
+    @NotEmpty // validacion. al menos un caracter
+    private  String correo;
+
+    //notacion para la fecha
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) //   formato iso, dia /mes /año
+    @Past //fechas pasadas, validacion.
+    @NotNull(message = "Ingrese fecha de nacimiento: ")
+    private LocalDate fechanac; //localdate tipo de dato. fechas
+    private LocalDateTime fechaRegistro;//feche y hora del registro actual cuando se entra a la web
+    //se configura con un metodo (esta al final)
+
+
+    //getter/setter
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public LocalDate getFechanac() {
+        return fechanac;
+    }
+
+    public void setFechanac(LocalDate fechanac) {
+        this.fechanac = fechanac;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @PrePersist
+    public void asignarFechaRegistro(){
+        fechaRegistro = LocalDateTime.now();
+    }
+
+    //metodo de fecharegistro. toma la hora actual de la pc
+    @PrePersist //para que funcione correctamente, asigna fecha actual
+    public void asignarfechareg(){
+        fechaRegistro = LocalDateTime.now();
+    }
+
 }
--- Agenda-contactos/src/main/java/com/example/Agendacontactos/servicio/ImpContactoServ.java
+++ Agenda-contactos/src/main/java/com/example/Agendacontactos/servicio/ImpContactoServ.java
@@ -1,2 +1,46 @@
-package com.example.Agendacontactos.servicio;public class ImpContactoServ {
+package com.example.Agendacontactos.servicio;
+
+import com.example.Agendacontactos.entidad.contacto;
+import com.example.Agendacontactos.repositorio.contactoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+//la logica esta en controlador
+//notacion servicio, traer implementacion de metodos crud de contactoserv
+@Service
+public class ImpContactoServ implements contactoServ{
+    //se recuperan los datos y se arma el cuerpo de los metodos
+
+    @Autowired
+    private contactoRepo contactoRepo;
+
+
+    @Override
+    //una lista del objeto <contacto> que muestre todo findall
+    public List<contacto> listarTodos() {
+        return contactoRepo.findAll();
+    }
+
+    @Override
+    public contacto guardarContacto(contacto contacto) {
+        return contactoRepo.save(contacto);
+    }
+
+    @Override
+    public contacto obtenerPorId(Integer id) {
+        return contactoRepo.findById(id).get();
+
+    }
+
+    @Override
+    public contacto actualizatContacto(contacto contacto) {
+        return contactoRepo.save(contacto);
+    }
+
+    @Override
+    public void eliminarContacto(contacto contacto) {
+
+        contactoRepo.delete(contacto);
+    }
 }
