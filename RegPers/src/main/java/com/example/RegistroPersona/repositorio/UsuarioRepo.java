a9bada6e5ae4daa5dd83511cb5b4a111846882af
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2023-06-05 22:01:48 -0300

    git commit -m "servicio"

--- RegPers/src/main/java/com/example/RegistroPersona/repositorio/UsuarioRepo.java
+++ RegPers/src/main/java/com/example/RegistroPersona/repositorio/UsuarioRepo.java
@@ -1,2 +1,14 @@
-package com.example.RegistroPersona.repositorio;public interface UsuarioRepo {
+package com.example.RegistroPersona.repositorio;
+
+import com.example.RegistroPersona.entidad.Usuario;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepo {
+    static Usuario save(Usuario usuario) {
+        //este metodo me lo desde usuarioservimp para "save" usuario no se porque
+        return usuario;
+    }
+
+    public Usuario findByEmail(String correo);
 }
