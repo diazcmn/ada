c22c70b08a8c5ef053a98d12c9ecd06ecc4cb929
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2023-06-30 14:13:35 -0300

    se agrega diagrama

--- ProyectoFinalAda-LibreriaJavaSpring/src/main/java/com/example/ProyectoFinalAdaLibreriaJavaSpring/controlador/libroControlador.java
+++ ProyectoFinalAda-LibreriaJavaSpring/src/main/java/com/example/ProyectoFinalAdaLibreriaJavaSpring/controlador/libroControlador.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import java.security.Principal;
 import java.util.List;
 
 @Controller
